77b0b649203e5545a6fca99586a887945bb8f797
ddg999 <slowman918@gmail.com>
2024-04-19 14:55:06 +0900

    중복제거

--- src/basic/ch13/LottoGame3.java
+++ src/basic/ch13/LottoGame3.java
@@ -10,7 +10,8 @@ public class LottoGame3 {
 		int gameNumber4 = LottoNumberMaker.makeNumber();
 		int gameNumber5 = LottoNumberMaker.makeNumber();
 		int gameNumber6 = LottoNumberMaker.makeNumber();
-		
+
+		// 출력
 		System.out.print(gameNumber1 + "\t");
 		System.out.print(gameNumber2 + "\t");
 		System.out.print(gameNumber3 + "\t");
@@ -18,8 +19,107 @@ public class LottoGame3 {
 		System.out.print(gameNumber5 + "\t");
 		System.out.print(gameNumber6 + "\t");
 		System.out.println();
-		System.out.println("----------------------------------------------");
+		System.out.println("-------------------------------------------");
 
+		// 중복 제거
+		while (gameNumber1 == gameNumber2 || gameNumber1 == gameNumber3 || gameNumber1 == gameNumber4
+				|| gameNumber1 == gameNumber5 || gameNumber1 == gameNumber6 || gameNumber2 == gameNumber3
+				|| gameNumber2 == gameNumber4 || gameNumber2 == gameNumber5 || gameNumber2 == gameNumber6
+				|| gameNumber3 == gameNumber4 || gameNumber3 == gameNumber5 || gameNumber3 == gameNumber6
+				|| gameNumber4 == gameNumber5 || gameNumber4 == gameNumber6 || gameNumber5 == gameNumber6) {
+
+			gameNumber2 = LottoNumberMaker.makeNumber();
+			gameNumber3 = LottoNumberMaker.makeNumber();
+			gameNumber4 = LottoNumberMaker.makeNumber();
+			gameNumber5 = LottoNumberMaker.makeNumber();
+			gameNumber6 = LottoNumberMaker.makeNumber();
+		}
+
+		// 출력
+		System.out.print(gameNumber1 + "\t");
+		System.out.print(gameNumber2 + "\t");
+		System.out.print(gameNumber3 + "\t");
+		System.out.print(gameNumber4 + "\t");
+		System.out.print(gameNumber5 + "\t");
+		System.out.print(gameNumber6 + "\t");
+		System.out.println();
+		System.out.println("-------------------------------------------");
+
+//		System.out.print(gameNumber1 + "\t");
+//		System.out.print(gameNumber2 + "\t");
+//		System.out.print(gameNumber3 + "\t");
+//		System.out.print(gameNumber4 + "\t");
+//		System.out.print(gameNumber5 + "\t");
+//		System.out.print(gameNumber6 + "\t");
+//		System.out.println();
+//		System.out.println("----------------------------------------------");
+//
+//		for (int i = 1; i <= 5; i++) {
+//			if (gameNumber1 > gameNumber2) {
+//				int tempBox = gameNumber1;
+//				gameNumber1 = gameNumber2;
+//				gameNumber2 = tempBox;
+//			}
+//			if (gameNumber2 > gameNumber3) {
+//				int tempBox = gameNumber2;
+//				gameNumber2 = gameNumber3;
+//				gameNumber3 = tempBox;
+//			}
+//			if (gameNumber3 > gameNumber4) {
+//				int tempBox = gameNumber3;
+//				gameNumber3 = gameNumber4;
+//				gameNumber4 = tempBox;
+//			}
+//			if (gameNumber4 > gameNumber5) {
+//				int tempBox = gameNumber4;
+//				gameNumber4 = gameNumber5;
+//				gameNumber5 = tempBox;
+//			}
+//			if (gameNumber5 > gameNumber6) {
+//				int tempBox = gameNumber5;
+//				gameNumber5 = gameNumber6;
+//				gameNumber6 = tempBox;
+//			}
+//		}
+//
+//		System.out.print(gameNumber1 + "\t");
+//		System.out.print(gameNumber2 + "\t");
+//		System.out.print(gameNumber3 + "\t");
+//		System.out.print(gameNumber4 + "\t");
+//		System.out.print(gameNumber5 + "\t");
+//		System.out.print(gameNumber6 + "\t");
+//		System.out.println();
+//		System.out.println("----------------------------------------------");
+//
+//		for (int i = 1; i <= 5; i++) {
+//			while (gameNumber1 == gameNumber2) {
+//				gameNumber2 = LottoNumberMaker.makeNumber();
+//			}
+//			while (gameNumber2 == gameNumber3) {
+//				gameNumber3 = LottoNumberMaker.makeNumber();
+//			}
+//			while (gameNumber3 == gameNumber4) {
+//				gameNumber4 = LottoNumberMaker.makeNumber();
+//			}
+//			while (gameNumber4 == gameNumber5) {
+//				gameNumber5 = LottoNumberMaker.makeNumber();
+//			}
+//			while (gameNumber5 == gameNumber6) {
+//				gameNumber6 = LottoNumberMaker.makeNumber();
+//			}
+//
+//		}
+//
+//		System.out.print(gameNumber1 + "\t");
+//		System.out.print(gameNumber2 + "\t");
+//		System.out.print(gameNumber3 + "\t");
+//		System.out.print(gameNumber4 + "\t");
+//		System.out.print(gameNumber5 + "\t");
+//		System.out.print(gameNumber6 + "\t");
+//		System.out.println();
+//		System.out.println("----------------------------------------------");
+
+		// 오름차순
 		for (int i = 1; i <= 5; i++) {
 			if (gameNumber1 > gameNumber2) {
 				int tempBox = gameNumber1;
@@ -48,6 +148,7 @@ public class LottoGame3 {
 			}
 		}
 
+		// 출력
 		System.out.print(gameNumber1 + "\t");
 		System.out.print(gameNumber2 + "\t");
 		System.out.print(gameNumber3 + "\t");
@@ -55,70 +156,7 @@ public class LottoGame3 {
 		System.out.print(gameNumber5 + "\t");
 		System.out.print(gameNumber6 + "\t");
 		System.out.println();
-		System.out.println("----------------------------------------------");
-
-		for (int i = 1; i <= 5; i++) {
-			while (gameNumber1 == gameNumber2) {
-				gameNumber2 = LottoNumberMaker.makeNumber();
-			}
-			while (gameNumber2 == gameNumber3) {
-				gameNumber3 = LottoNumberMaker.makeNumber();
-			}
-			while (gameNumber3 == gameNumber4) {
-				gameNumber4 = LottoNumberMaker.makeNumber();
-			}
-			while (gameNumber4 == gameNumber5) {
-				gameNumber5 = LottoNumberMaker.makeNumber();
-			}
-			while (gameNumber5 == gameNumber6) {
-				gameNumber6 = LottoNumberMaker.makeNumber();
-			}
-
-		}
-
-		System.out.print(gameNumber1 + "\t");
-		System.out.print(gameNumber2 + "\t");
-		System.out.print(gameNumber3 + "\t");
-		System.out.print(gameNumber4 + "\t");
-		System.out.print(gameNumber5 + "\t");
-		System.out.print(gameNumber6 + "\t");
-		System.out.println();
-		System.out.println("----------------------------------------------");
-
-		for (int i = 1; i <= 5; i++) {
-			if (gameNumber1 > gameNumber2) {
-				int tempBox = gameNumber1;
-				gameNumber1 = gameNumber2;
-				gameNumber2 = tempBox;
-			}
-			if (gameNumber2 > gameNumber3) {
-				int tempBox = gameNumber2;
-				gameNumber2 = gameNumber3;
-				gameNumber3 = tempBox;
-			}
-			if (gameNumber3 > gameNumber4) {
-				int tempBox = gameNumber3;
-				gameNumber3 = gameNumber4;
-				gameNumber4 = tempBox;
-			}
-			if (gameNumber4 > gameNumber5) {
-				int tempBox = gameNumber4;
-				gameNumber4 = gameNumber5;
-				gameNumber5 = tempBox;
-			}
-			if (gameNumber5 > gameNumber6) {
-				int tempBox = gameNumber5;
-				gameNumber5 = gameNumber6;
-				gameNumber6 = tempBox;
-			}
-		}
-
-		System.out.print(gameNumber1 + "\t");
-		System.out.print(gameNumber2 + "\t");
-		System.out.print(gameNumber3 + "\t");
-		System.out.print(gameNumber4 + "\t");
-		System.out.print(gameNumber5 + "\t");
-		System.out.print(gameNumber6 + "\t");
+		System.out.println("-------------------------------------------");
 
 	} // end of main
 
