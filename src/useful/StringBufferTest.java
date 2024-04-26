4a31366963b78f92db8bc226396ec1c8c16b44c8
ddg999 <slowman918@gmail.com>
2024-04-26 10:35:49 +0900

    string buffer

---
+++ src/useful/StringBufferTest.java
@@ -0,0 +1,28 @@
+package useful;
+
+public class StringBufferTest {
+
+	public static void main(String[] args) {
+
+		String str1 = new String("Hello");
+		String str2 = new String("World");
+
+		StringBuffer bufferStr = new StringBuffer("Hello");
+		System.out.println(bufferStr);
+		System.out.println(System.identityHashCode(bufferStr)); // 원시 주소값
+
+		bufferStr.append(str2);
+		System.out.println(bufferStr);
+		System.out.println(System.identityHashCode(bufferStr));
+
+		// 1번 결과와 2번 결과에 주소값이 같다라는 의미는
+		// 새로운 메모리를 할당 한 것이 아닌 변경한 것이다.
+
+		// 활용
+		String newStr = bufferStr.toString(); // toString 호출시 타입 -> String
+
+		System.out.println(newStr);
+
+	} // end of main
+
+}
